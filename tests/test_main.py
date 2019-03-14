c85ab96e23e3a7a75f02f8d1090cfeb1ccdb2e4d
bsipin20 <briansipin@Brians-MacBook-Pro.local>
2019-03-13 20:24:27 -0400

    adding test main

---
+++ tests/test_main.py
@@ -0,0 +1,26 @@
+import unittest
+import unittest.mock as mock
+import sys
+import os
+
+sys.path.append(os.path.abspath('..'))
+from cashback.solve import _calc_cashback
+
+
+class TestMain(unittest.TestCase): 
+
+    def test_main(self):
+        test_data = {
+            'dining' : [
+            v
+
+    
+
+
+
+        }
+
+
+    
+
+
